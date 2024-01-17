d60cef8806d5b7d0021030e3fe4d5bcfa1f7fe41
joelsantos-js <joeltere9@gmail.com>
2024-01-17 17:00:25 -0300

    create user and role

---
+++ src/main/java/com/joel/br/Parkapi/ParkApiApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.Parkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkApiApplication.class, args);
+	}
+
+}
